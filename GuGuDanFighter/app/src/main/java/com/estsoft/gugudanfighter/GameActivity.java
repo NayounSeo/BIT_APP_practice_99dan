269bc9496e7489a9171172ce26f528e749c5e90b
NayounSeo <parpe94@naver.com>
2016-05-24 10:24:04 +0900

    구구단 끝...

--- GuGuDanFighter/app/src/main/java/com/estsoft/gugudanfighter/GameActivity.java
+++ GuGuDanFighter/app/src/main/java/com/estsoft/gugudanfighter/GameActivity.java
@@ -42,7 +42,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int viewId = v.getId();
-        System.out.println("누른 버튼은 : "+viewId);
         if (viewId == buttons[hereIsTheResult].getId()) {
             gameCorrect++;
         } else {
@@ -71,19 +70,37 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             int random2 = randomize(1, 9);
             int tempRandom = random1*random2;
             System.out.println("tempRandom : "+ tempRandom+", result : "+result);
+            System.out.println();
+            for( int j=0; j<9; j++) {
+                System.out.print(randomAnswers[j]+"  ");
+            }
+            System.out.println();
             Boolean isAlready = whatArrayContains(randomAnswers, tempRandom, result);
             if(isAlready) {
-                System.out.println("isAlready에 들어온 아이는 : "+(i+1)+"번째");
                 //TODO
                 /* 다 짰는데 그리고 중복검사도 하는데
                 * 왜 중복되는 숫자가 생겨있나 했더니ㅜㅠㅜㅠㅜ
                 * 여기 들어와도 i가 줄어들지 않아서,
                 * 초기화 되지 못하는 버튼이 생기는 거였다!*/
                 i -= 1;
-                /*whatArrayContains 함수는 다시 처음처럼 고쳐주고,
+                /* 그러고서 나는 무한 루프에 대해서는....
+                * whatArrayContains 함수는 다시 처음처럼 고쳐주고,
                 * 정답에 대한 중복검사는 끝에 한번만 하게 고치는 것으로!
                 * TH 예닮
+                * ===========================================
+                * 16년 5월 24일
+                * 위 방식대로 하려면 결국 정답이 어디 저장되어 있는지 또 반환해야해서..ㅜㅠ
+                * 원래 하던대로 하려고 하는데
+                * tempRandom의 값과 result가 같으면 그 이후에 계속 무한루프를 도는 거였다!
+                * 왜람? 그리고 이걸 어떻게 처리해야 하지?
+                * 아... 예닮이가 한 말이 뭔지 알것도 같음
+                * ☆☆☆배열 찍은 걸 보니까!! 정답이 벌써 들어가 버렸어 왜지..?ㅜㅠㅜㅠㅜㅠㅜㅠㅋㅋㅋㅋㅋㅋㅋ
+                * 그래서 아래쪽에 새 if문과 continue를 써 주었습니다.
                 * */
+                continue;
+            }
+            /*추가 결과 드디어 멀쩡하게 돌아갑니다!!!♡♡♡♡♡♡*/
+            if ( tempRandom == result ) {
                 continue;
             }
             randomAnswers[i] = tempRandom;
@@ -98,7 +115,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         return hereIsTheResult;
     }
 
-    private boolean whatArrayContains( int[] array, int val1, int val2 ) {
+    private boolean whatArrayContains( int[] array, int val1, int val2) {
         for( final int i : array ) {
             if( i == val1 || i == val2) {
                 return true;
